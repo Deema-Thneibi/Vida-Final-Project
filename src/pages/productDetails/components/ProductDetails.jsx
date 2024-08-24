2f57fa236b4217fb658a9e2fee395ab1ba4102a5
Deema-Thnabi <deemathnabi25@gmail.com>
2024-08-24 21:08:26 +0300

    first commit

--- src/pages/productDetails/components/ProductDetails.jsx
+++ src/pages/productDetails/components/ProductDetails.jsx
@@ -4,12 +4,10 @@ import { useEffect, useState } from "react";
 import Loader from "../../../components/loader/components/Loader";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTruck } from "@fortawesome/free-solid-svg-icons";
- import StarRating from "./starRating/StarRating.jsx";
- import Cookie from  'cookie-universal';
+import StarRating from "./starRating/StarRating.jsx";
+import Cookie from 'cookie-universal';
 import "./productDetails.css";
 import { Slide, toast } from 'react-toastify';
-
-
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -20,7 +18,7 @@ import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
-// import required modules
+// Import required modules
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 
 export default function ProductDetails() {
@@ -50,22 +48,50 @@ export default function ProductDetails() {
   }, []);
 
   const addToCart = async (id) => {
-    const cookie =  Cookie();
+    const cookie = Cookie();
     const token = cookie.get('userToken');
-    if(token){
-    try{
-      setIsAddedToCart(true);
-      const {data} = await axios.post(`${import.meta.env.VITE_API}/cart`,
-        {
-          productId:id
-        },
-        {
-          headers: {
-            Authorization: `Tariq__${token}`
+    if (token) {
+      try {
+        setIsAddedToCart(true);
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API}/cart`,
+          {
+            productId: id
+          },
+          {
+            headers: {
+              Authorization: `Tariq__${token}`
+            }
           }
-        }
-      )
-      toast.success('Product added to cart successfully', {
+        );
+        toast.success('Product added to cart successfully', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          transition: Slide,
+        });
+      } catch (error) {
+        toast.error(error.response.data.message, {
+          position: "top-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          transition: Slide,
+        });
+      } finally {
+        setIsAddedToCart(false);
+      }
+    } else {
+      toast.info('Please signin to add products to cart', {
         position: "top-right",
         autoClose: 5000,
         hideProgressBar: false,
@@ -75,39 +101,11 @@ export default function ProductDetails() {
         progress: undefined,
         theme: "colored",
         transition: Slide,
-        });
-    }catch(error){
-      toast.error(error.response.data.message, {
-        position: "top-right",
-        autoClose: 3000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-        transition: Slide,
-        });
-    }finally {
+      });
       setIsAddedToCart(false);
     }
-  } 
+  };
 
-else {
-  toast.info('Please signin to add products to cart', {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-    transition: Slide,
-    });
-    setIsAddedToCart(false);
-}
-  }
   if (isLoader) {
     return <Loader />;
   }
@@ -116,93 +114,94 @@ else {
     <>
       <div className="productPage">
         <div className="container">
-        {error?<div className='error container'><h2>{error}</h2></div>:
-          <div className="productSections row">
-            <div className="productPageUpSection">
-              <div className="productImages">
-                <Swiper
-                  onSwiper={setThumbsSwiper}
-                  spaceBetween={10}
-                  slidesPerView={4}
-                  direction="vertical"
-                  freeMode={true}
-                  watchSlidesProgress={true}
-                  modules={[FreeMode, Navigation, Thumbs]}
-                  className="mySwiper"
-                >
-                  <SwiperSlide>
-                    <img src={product.mainImage.secure_url} alt={product.name} />
-                  </SwiperSlide>
-                  {product.subImages.map((subImage) => (
-                    <SwiperSlide key={subImage._id}>
-                      <img src={subImage.secure_url} alt={product.name} />
+          {error ? (
+            <div className='error container'>
+              <h2>{error}</h2>
+            </div>
+          ) : (
+            <div className="productSections row">
+              <div className="productPageUpSection">
+                <div className="productImages">
+                  <Swiper
+                    onSwiper={setThumbsSwiper}
+                    spaceBetween={10}
+                    slidesPerView={4}
+                    direction="vertical"
+                    freeMode={true}
+                    watchSlidesProgress={true}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper"
+                  >
+                    <SwiperSlide key={product.mainImage._id}>
+                      <img src={product.mainImage.secure_url} alt={product.name} />
                     </SwiperSlide>
-                  ))}
-                </Swiper>
-                <Swiper
-                style={{
-                  "--swiper-navigation-color": "#fff",
-                  "--swiper-pagination-color": "#fff",
-                }}
-                spaceBetween={10}
-                navigation={true}
-                thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
-                modules={[FreeMode, Navigation, Thumbs]}
-                className="mySwiper2"
-              >
-                <SwiperSlide>
-                  <img src={product.mainImage.secure_url} alt={product.name} />
-                </SwiperSlide>
-                {product.subImages.map((subImage) => (
-                  <SwiperSlide key={subImage._id}>
-                    <img src={subImage.secure_url} alt={product.name} />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-              </div>
-              <div className="productInfo">
-              <h2>{product.name}</h2>
-              {product.discount <= 0 ? (
-                <div className="productPrice">
-                  <p className="finalPrice"> ${product.finalPrice}</p>
+                    {product.subImages.map((subImage) => (
+                      <SwiperSlide key={subImage._id}>
+                        <img src={subImage.secure_url} alt={product.name} />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                  <Swiper
+                    style={{
+                      "--swiper-navigation-color": "#fff",
+                      "--swiper-pagination-color": "#fff",
+                    }}
+                    spaceBetween={10}
+                    navigation={true}
+                    thumbs={thumbsSwiper ? { swiper: thumbsSwiper } : undefined}
+                    modules={[FreeMode, Navigation, Thumbs]}
+                    className="mySwiper2"
+                  >
+                    <SwiperSlide key={product.mainImage._id}>
+                      <img src={product.mainImage.secure_url} alt={product.name} />
+                    </SwiperSlide>
+                    {product.subImages.map((subImage) => (
+                      <SwiperSlide key={subImage._id}>
+                        <img src={subImage.secure_url} alt={product.name} />
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
                 </div>
-              ) : (
-                <div className="productPrice">
-                  <p className="price"> ${product.price}</p>
-                  <p className="finalPrice"> ${product.finalPrice}</p>
-                  <p className="discount"> {product.discount}% off</p>
-                </div>
-              )}
-              <div className="shipping">
-                <FontAwesomeIcon icon={faTruck} />
-                <p>2 days Shipping</p>
-              </div>
-              <div className="addToCart">
-                <button onClick={()=>addToCart(product._id)}    disabled={isAddedToCart??'disabled'}>Add to Cart</button>
-              </div>
-            </div>
-            </div>
-            <div className="productPageDownSection">
-          <div className="productReviews">
-            <h2>Reviews</h2>
-            {
-              product.reviews.map(review =>
-                <>
-                <div key={review._id} className="review">
-                  <div className="rating">
-                  <p>{review.createdBy.userName}</p>
-                  <StarRating rating={review.rating} /> 
+                <div className="productInfo">
+                  <h2>{product.name}</h2>
+                  {product.discount <= 0 ? (
+                    <div className="productPrice">
+                      <p className="finalPrice"> ${product.finalPrice}</p>
+                    </div>
+                  ) : (
+                    <div className="productPrice">
+                      <p className="price"> ${product.price}</p>
+                      <p className="finalPrice"> ${product.finalPrice}</p>
+                      <p className="discount"> {product.discount}% off</p>
+                    </div>
+                  )}
+                  <div className="shipping">
+                    <FontAwesomeIcon icon={faTruck} />
+                    <p>2 days Shipping</p>
                   </div>
-                  <p>{review.comment}</p>
+                  <div className="addToCart">
+                    <button onClick={() => addToCart(product._id)} disabled={isAddedToCart}>
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
-                </>
-              )
-            }
-          </div>
-          </div>
-
-          </div>
-}
+              </div>
+              <div className="productPageDownSection">
+                <div className="productReviews">
+                  <h2>Reviews</h2>
+                  {product.reviews.map((review) => (
+                    <div key={review._id} className="review">
+                      <div className="rating">
+                        <p>{review.createdBy.userName}</p>
+                        <StarRating rating={review.rating} />
+                      </div>
+                      <p>{review.comment}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
